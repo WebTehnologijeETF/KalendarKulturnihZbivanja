dfcb607339e51b730ed38151a8d632e1d2a3ed94
aklacar1 <aklacar1@etf.unsa.ba>
2015-04-15 14:45:23 +0200

    Validacija Verzija1

--- JS/js_1.js
+++ JS/js_1.js
@@ -1,5 +1,4 @@
 function showSubMenu(test, id) {
-
     var x = document.getElementById(id);
     if (test == 1) {
         x.style.display = "block";
@@ -8,3 +7,177 @@ function showSubMenu(test, id) {
         x.style.display = "none";
     }
 }
+
+
+var x = false;
+var textForOutput = "";
+
+
+function validateForm() {
+    changeTextOutput();
+    if (textForOutput != "") {
+        alert(textForOutput);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function changeTextOutput() {
+    textForOutput = "";
+    validate("ime");
+    if (!x) {
+        textForOutput += "Ime je obavezno !" + "\n";
+    }
+    validate("prezime");
+    if (!x) {
+        textForOutput += "Prezime je obavezno !" + "\n";
+    }
+    validate("emailA");
+    if (!x) {
+        textForOutput += "Format EMaila je : email@example.com !" + "\n";
+    }
+    validate("emailB");
+    if (!x) {
+        textForOutput += "EMail mora biti isti u oba polja !" + "\n";
+    }
+    validate("spolLista");
+    if (!x) {
+        textForOutput += "Spol (Muško,Žensko ili Drugo)!" + "\n";
+    }
+    validate("bday");
+    if (!x) {
+        textForOutput += "Datum Rođenja je obavezan i mora biti ispod godine 2000!" + "\n" + "Format datuma je mm/dd/yyyy";
+    }
+}
+function validate(id) {
+
+    var y = document.getElementById(id);
+    var z = y.parentNode;
+    var p = z.children[1];
+    if (id == "ime" || id == "prezime" || id == "bday") {
+        validateString(y, p);
+    }
+    else if (id == "spolLista") {
+        validateSpolLista(y, z.children[2]);
+    }
+    else if (id == "emailA" || id == "emailB") {
+        validateEMail();
+    }
+}
+
+
+function validateString(y, p) {
+
+    if (y.id == "bday" && y.value >= "2000-01-01" && (y.value != "" || y.value != null)) {
+        x = false;
+        p.style.width = "21px";
+        p.style.height = "21px";
+    }
+    else {
+        if (y.value == "") {
+            p.style.width = "21px";
+            p.style.height = "21px";
+            x = false;
+        }
+        else {
+            p.style.width = "0";
+            p.style.height = "0";
+            x = true;
+        }
+    }
+}
+
+
+function validateSpolLista(y, p) {
+    if (y.value != "Muško" && y.value != "Žensko" && y.value != "Drugo") {
+        p.style.width = "21px";
+        p.style.height = "21px";
+        x = false;
+    }
+    else {
+        p.style.width = "0";
+        p.style.height = "0";
+        x = true;
+    }
+
+
+}
+
+function validateEMail() {
+
+    var yA = document.getElementById("emailA");
+    var zA = yA.parentNode;
+    var pA = zA.children[1];
+
+    var yB = document.getElementById("emailB");
+    var zB = yB.parentNode;
+    var pB = zB.children[1];
+
+    var re = /(\w+)@((\w+).){2}.(\w+){2}/i;
+
+    if (!re.test(yA.value) || !re.test(yB.value) || yA.value != yB.value || yA.value == "" || yB.value == "") {
+        x = false;
+        pA.style.width = "21px";
+        pA.style.height = "21px";
+        pB.style.width = "21px";
+        pB.style.height = "21px";
+    }
+    else {
+        x = true;
+        pA.style.width = "0";
+        pA.style.height = "0";
+        pB.style.width = "0";
+        pB.style.height = "0";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
