ea3aa18e246901bf505518fe0588dc816b44da9a
aklacar1 <aklacar1@etf.unsa.ba>
2015-05-03 11:39:13 +0200

    Single Page Application

--- JS/js_1.js
+++ JS/js_1.js
@@ -133,7 +133,18 @@ function validateEMail() {
 }
 
 
+function ucitaj(id) {
+    var ajax = new XMLHttpRequest();
 
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.getElementById("Page").innerHTML = ajax.responseText;
+        }
+    };
+
+    ajax.open("GET", "HTML/" + id + ".html", true);
+    ajax.send();
+}
 
 
 
